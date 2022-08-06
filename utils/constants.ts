36597c8f6bcf590dc9833ef78d2ce3c150e23739
Roy Kachani <roy.kachani@gmail.com>
2022-08-06 01:18:20 -0300

    utils

---
+++ utils/constants.ts
@@ -0,0 +1,33 @@
+export const SORT = {
+  ASC: 'asc',
+  DESC: 'desc',
+  OLD: 'old',
+  NEW: 'new',
+};
+
+export const PICT = [
+  {
+    name: 'Return of the Jedi',
+    img: 'assets/Star-Wars-Return-Jedi-ep6.webp',
+  },
+  {
+    name: 'The Empire Strikes Back',
+    img: 'assets/Star-Wars-Empire-Strikes-Back-ep5.webp',
+  },
+  {
+    name: 'A New Hope',
+    img: 'assets/Star-Wars-New-Hope-ep4.webp',
+  },
+  {
+    name: 'The Phantom Menace',
+    img: 'assets/Star-Wars-Phantom-Menace-ep1.webp',
+  },
+  {
+    name: 'Attack of the Clones',
+    img: 'assets/Star-Wars-Attack-Clones-ep2.webp',
+  },
+  {
+    name: 'Revenge of the Sith',
+    img: 'assets/Star-Wars-Revenge-Sith-ep3.webp',
+  },
+];
